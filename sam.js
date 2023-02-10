e086ac3926723b3d15e119d95803924bd5e7c996
chandru90 <iochandru90@gmail.com>
2023-02-10 13:58:33 +0530

    commit 9

--- sam.js
+++ sam.js
@@ -1,55 +1,85 @@
 var form =document.getElementById('addform')
-form.addEventListener('submit',additem)
-function additem(e)
+window.form.addEventListener('submit',additem)
+
+async function sync(){
+    await  additems(respone.data)
+    
+    printelement(obj)
+    console.log("async executed")
+}
+sync ()
+
+ function additem(e)
 {
     e.preventDefault();
     var amount1 =document.getElementById('amount').value ;
     var description1 =document.getElementById('description').value;
     var category=document.getElementById('category').value
     const obj ={amount1,description1,category}
-    axios.post("https://crudcrud.com/api/64de2d79843c491dadfb3910b5420d3b/appointment",obj)
+ 
+     axios.post("https://crudcrud.com/api/930f181238ec40e299dc22b861de0683/appointment/",obj)
     .then((response)=>{
-        console.log(response)
+        console.log(response.data)
     })
-    .catch((err)=>{
+    .catch((err)=>{decodeURIComponent
         console.log(err)
     })
 
-   
+    //console.log(response.data._uid)
 
     //  localStorage.setItem(obj.amount1,JSON.stringify(obj))
-    printelement(obj)
+ printelement(obj)
 }
 
+//console.log(obj._uid)
 
 function printelement(obj)
 {
+   // console.log(obj._uid)
 const parentelement =document.getElementById('listitems')
 const childelement=document.createElement('li')
 childelement.textContent= obj.amount1 + '- '+ obj.description1 +'-'+obj.category
+
 const editbtn =document.createElement('input')
 editbtn.type ="button"
 editbtn.value='edit'
-editbtn.onclick =()=>
+editbtn.onclick =(obj)=>
 {
-   localStorage.removeItem(obj.amount)
+    axios.put("https://crudcrud.com/api/930f181238ec40e299dc22b861de0683/appointment/",obj)
+    localStorage.removeItem(obj.amount)
     parentelement.removeChild(childelement)
     document.getElementById('amount').value =obj.amount1
     document.getElementById('description').value=obj.description1
    document.getElementById('category').value=obj.category
 
 }
+
+
 const deletebtn =document.createElement('input')
 deletebtn.type="button"
 deletebtn.value='delete'
-deletebtn.onclick =() =>
+deletebtn.onclick =(obj) =>
 {
+    axios({
+        method :'delete',url:'https://crudcrud.com/api/930f181238ec40e299dc22b861de0683/appointment/',
+        data : {amount :obj.amount1 ,completed : true} })
+       .then(res=>showOutput(res))
+       .catch(err =>console.error(err))
+ 
+
+  /*  axios.delete("https://crudcrud.com/api/930f181238ec40e299dc22b861de0683/appointment/",{obj})
+    .then((response)=>{
+        console.log(response.data)
+    })
+    .catch((err)=>{decodeURIComponent
+        console.log(err)
+    })*/
   localStorage.removeItem(obj.amount)
     parentelement.removeChild(childelement)
 }
+
     childelement.appendChild(editbtn)
     childelement.appendChild(deletebtn)
     parentelement.appendChild(childelement)
 
 }
-
