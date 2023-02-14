1b2a869098b8235d32c3eff02a3e8b27680d1159
chandru90 <iochandru90@gmail.com>
2023-02-14 08:59:57 +0530

    tenth commit

--- sam.js
+++ sam.js
@@ -1,85 +1,116 @@
 var form =document.getElementById('addform')
-window.form.addEventListener('submit',additem)
-
-async function sync(){
-    await  additems(respone.data)
-    
-    printelement(obj)
-    console.log("async executed")
-}
-sync ()
-
+form.addEventListener('submit',additem)
  function additem(e)
 {
     e.preventDefault();
+   
     var amount1 =document.getElementById('amount').value ;
     var description1 =document.getElementById('description').value;
     var category=document.getElementById('category').value
     const obj ={amount1,description1,category}
+    
  
-     axios.post("https://crudcrud.com/api/930f181238ec40e299dc22b861de0683/appointment/",obj)
+     axios.post("https://crudcrud.com/api/7cb395bff3854faf9ae25395e8f25df4/appointment/",obj)
     .then((response)=>{
-        console.log(response.data)
+        for(var i=0;i<response.data.length;i++)
+        {
+           printelement(response.data[i])
+            console.log(response.data[i])
+            
+        } 
+        return new Promise((res,rej)=>
+{setTimeout(()=>{res(response.data)},1000)})
+       
+      //  console.log(response)
+       
     })
-    .catch((err)=>{decodeURIComponent
-        console.log(err)
+    .catch((err)=>{
+     console.log(err)
     })
+
+    
 
     //console.log(response.data._uid)
 
     //  localStorage.setItem(obj.amount1,JSON.stringify(obj))
- printelement(obj)
-}
 
-//console.log(obj._uid)
+    printelement(obj)
+   
+
 
 function printelement(obj)
 {
-   // console.log(obj._uid)
-const parentelement =document.getElementById('listitems')
-const childelement=document.createElement('li')
-childelement.textContent= obj.amount1 + '- '+ obj.description1 +'-'+obj.category
+    //console.log(obj1._uid)
 
+ 
+   const parentelement =document.getElementById('listitems')
+   const childelement=document.createElement('li')
+   childelement.textContent= obj.amount1 + '- '+ obj.description1 +'-'+obj.category
 const editbtn =document.createElement('input')
 editbtn.type ="button"
 editbtn.value='edit'
-editbtn.onclick =(obj)=>
+editbtn.onclick =()=>
 {
-    axios.put("https://crudcrud.com/api/930f181238ec40e299dc22b861de0683/appointment/",obj)
+    editbtn1()
+}
+const deletebtn =document.createElement('input')
+deletebtn.type="button"
+deletebtn.value='delete'
+deletebtn.onclick =() =>
+{
+    deletebtn1()
+}
+
+
+
+
+childelement.appendChild(editbtn)
+childelement.appendChild(deletebtn)
+ parentelement.appendChild(childelement)
+
+
+
+function editbtn1(obj)
+{
+    
+    axios.put("https://crudcrud.com/api/7cb395bff3854faf9ae25395e8f25df4/appointment/",obj)
     localStorage.removeItem(obj.amount)
     parentelement.removeChild(childelement)
     document.getElementById('amount').value =obj.amount1
     document.getElementById('description').value=obj.description1
    document.getElementById('category').value=obj.category
+   
+
 
 }
 
 
-const deletebtn =document.createElement('input')
-deletebtn.type="button"
-deletebtn.value='delete'
-deletebtn.onclick =(obj) =>
-{
-    axios({
-        method :'delete',url:'https://crudcrud.com/api/930f181238ec40e299dc22b861de0683/appointment/',
-        data : {amount :obj.amount1 ,completed : true} })
-       .then(res=>showOutput(res))
-       .catch(err =>console.error(err))
- 
 
-  /*  axios.delete("https://crudcrud.com/api/930f181238ec40e299dc22b861de0683/appointment/",{obj})
+function deletebtn1()
+{
+   
+
+
+    axios.delete('https://crudcrud.com/api/7cb395bff3854faf9ae25395e8f25df4/appointment/${obj._id}')
     .then((response)=>{
         console.log(response.data)
     })
-    .catch((err)=>{decodeURIComponent
+    .catch((err)=>{
         console.log(err)
-    })*/
-  localStorage.removeItem(obj.amount)
-    parentelement.removeChild(childelement)
+    })
+ 
+
+ parentelement.removeChild(childelement)
+    
+    
+}
 }
 
-    childelement.appendChild(editbtn)
-    childelement.appendChild(deletebtn)
-    parentelement.appendChild(childelement)
-
+async function test ()
+{
+    const msg =await additems(e)
+    console.log(msg)
+}
+ 
+test()
 }
